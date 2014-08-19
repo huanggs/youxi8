6602c45a01d9c68d27f856da0017a9524cfed634
huanggs <gs.h2004@126.com>
2014-08-20 05:06:13 +0800

    mac_08200505
    Create

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<head>
+  <meta charset="utf-8" />
+  <title>首页 - 游戏吧</title>
+  <link rel="stylesheet" type="text/css" href="css/main.css">
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/jquery-1.11.1.min.js"></script>
+</head>
+
+<body>
+  <div class="container">
+    <div class="row">
+      <div class="span8"><h2>游戏吧</h3></div>
+      <div class="span4"><h5>关注游戏圈</h2></div>
+    </div>
+  </div>
+</body>
