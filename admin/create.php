633448561894630bfd4e9578748db90ba5858b59
huanggs <gs.h2004@126.com>
2014-08-26 23:21:52 +0800

    mac_08262321
    AdminPanel

--- admin/create.php
+++ admin/create.php
@@ -2,7 +2,9 @@
 <!DOCTYPE html>
 <head>
   <meta charset="utf-8" />
-  <title>录入信息 - 游戏吧</title>
+  <title>首页 - 游戏吧 - 掌握最全游戏信息</title>
+  <mete name="keyword" content="游戏,展会,电竞,测试,直播,英雄联盟,比赛">
+  <mete name="description" content="关注游戏吧，掌握最全游戏信息。游戏吧通过简洁的内容呈现，让玩家可以快速了解游戏行业重大事件，从此不再错过任何精彩游戏。">
   <link rel="stylesheet" type="text/css" href="../css/main.css">
   <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
   <script src="../js/bootstrap.min.js"></script>
