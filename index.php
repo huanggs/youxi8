8bb6f7126b62301a7942c3c09805fa59f746eea6
Ghost <gs.h2004@126.com>
2014-09-02 01:08:41 +0800

    mac_09020108_B_test

--- index.php
+++ index.php
@@ -13,6 +13,52 @@
     <?php include "inc/nav.php" ?>
     <div class="row">
       <div class="col-md-12">
+
+      <?php
+        for ($i=0;$i<9;$i++){
+          switch ($i) {
+            case '0':
+              $today=$today;
+              $today_w=$today_w;
+              break;
+            case '1':
+              $today=$today_1;
+              $today_w=$today_1_w;  
+              break;
+            case '2':
+              $today=$today_2;
+              $today_w=$today_2_w;  
+              break;
+            case '3':
+              $today=$today_3;
+              $today_w=$today_3_w;  
+              break;
+            case '4':
+              $today=$today_4;
+              $today_w=$today_4_w;  
+              break;
+            case '5':
+              $today=$today_5;
+              $today_w=$today_5_w;  
+              break;
+            case '6':
+              $today=$today_6;
+              $today_w=$today_6_w;  
+              break;
+            case '7':
+              $today=$today_7;
+              $today_w=$today_7_w;  
+              break;
+            case '8':
+              $today=$today_8;
+              $today_w=$today_8_w;  
+              break;
+            case '9':
+              $today=$today_9;
+              $today_w=$today_9_w;  
+              break;
+          }
+          ?>
         <div class="DayTitle">
           <span class="DayTitleDate"><?php echo "$today";?></span>
           <span class="DayTitleWeek"><?php echo "星期".$weekarray[$today_w];?></span>
@@ -27,132 +73,10 @@
             }
           ?>
         </table>
-        
-        <div class="DayTitle">
-          <span class="DayTitleDate"><?php echo "$today_1";?></span>
-          <span class="DayTitleWeek"><?php echo "星期".$weekarray[$today_1_w];?></span>
-        </div>
-        <table class="table table-hover table-condensed content">
-          <?php
-            $category = @$_GET['category'];
-            $query_content2=mysql_query("SELECT * FROM event WHERE category like '%$category%' and EventDay like '$today_1' ORDER BY EventDay");
-            while ($data_content=mysql_fetch_array($query_content2)) {
-            include "inc/event_index.php";
-            }
-          ?>
-        </table>
-        
-        <div class="DayTitle">
-          <span class="DayTitleDate"><?php echo "$today_2";?></span>
-          <span class="DayTitleWeek"><?php echo "星期".$weekarray[$today_2_w];?></span>
-        </div>
-        <table class="table table-hover table-condensed content">
-          <?php
-            $category = @$_GET['category'];
-            $query_content2=mysql_query("SELECT * FROM event WHERE category like '%$category%' and EventDay like '$today_2' ORDER BY EventDay");
-            while ($data_content=mysql_fetch_array($query_content2)) {
-            include "inc/event_index.php";
-            }
-          ?>
-        </table>
-        
-        <div class="DayTitle">
-          <span class="DayTitleDate"><?php echo "$today_3";?></span>
-          <span class="DayTitleWeek"><?php echo "星期".$weekarray[$today_3_w];?></span>
-        </div>
-        <table class="table table-hover table-condensed content">
-          <?php
-            $category = @$_GET['category'];
-            $query_content2=mysql_query("SELECT * FROM event WHERE category like '%$category%' and EventDay like '$today_3' ORDER BY EventDay");
-            while ($data_content=mysql_fetch_array($query_content2)) {
-            include "inc/event_index.php";
-            }
-          ?>
-        </table>
-        
-        <div class="DayTitle">
-          <span class="DayTitleDate"><?php echo "$today_4";?></span>
-          <span class="DayTitleWeek"><?php echo "星期".$weekarray[$today_4_w];?></span>
-        </div>
-        <table class="table table-hover table-condensed content">
-          <?php
-            $category = @$_GET['category'];
-            $query_content2=mysql_query("SELECT * FROM event WHERE category like '%$category%' and EventDay like '$today_4' ORDER BY EventDay");
-            while ($data_content=mysql_fetch_array($query_content2)) {
-            include "inc/event_index.php";
-            }
-          ?>
-        </table>
-        
-        <div class="DayTitle">
-          <span class="DayTitleDate"><?php echo "$today_5";?></span>
-          <span class="DayTitleWeek"><?php echo "星期".$weekarray[$today_5_w];?></span>
-        </div>
-        <table class="table table-hover table-condensed content">
-          <?php
-            $category = @$_GET['category'];
-            $query_content2=mysql_query("SELECT * FROM event WHERE category like '%$category%' and EventDay like '$today_5' ORDER BY EventDay");
-            while ($data_content=mysql_fetch_array($query_content2)) {
-            include "inc/event_index.php";
-            }
-          ?>
-        </table>
-        
-        <div class="DayTitle">
-          <span class="DayTitleDate"><?php echo "$today_6";?></span>
-          <span class="DayTitleWeek"><?php echo "星期".$weekarray[$today_6_w];?></span>
-        </div>
-        <table class="table table-hover table-condensed content">
-          <?php
-            $category = @$_GET['category'];
-            $query_content2=mysql_query("SELECT * FROM event WHERE category like '%$category%' and EventDay like '$today_6' ORDER BY EventDay");
-            while ($data_content=mysql_fetch_array($query_content2)) {
-            include "inc/event_index.php";
-            }
-          ?>
-        </table>
-        
-        <div class="DayTitle">
-          <span class="DayTitleDate"><?php echo "$today_7";?></span>
-          <span class="DayTitleWeek"><?php echo "星期".$weekarray[$today_7_w];?></span>
-        </div>
-        <table class="table table-hover table-condensed content">
-          <?php
-            $category = @$_GET['category'];
-            $query_content2=mysql_query("SELECT * FROM event WHERE category like '%$category%' and EventDay like '$today_7' ORDER BY EventDay");
-            while ($data_content=mysql_fetch_array($query_content2)) {
-            include "inc/content_index.php";
-            }
-          ?>
-        </table>
-        
-        <div class="DayTitle">
-          <span class="DayTitleDate"><?php echo "$today_8";?></span>
-          <span class="DayTitleWeek"><?php echo "星期".$weekarray[$today_8_w];?></span>
-        </div>
-        <table class="table table-hover table-condensed content">
-          <?php
-            $category = @$_GET['category'];
-            $query_content2=mysql_query("SELECT * FROM event WHERE category like '%$category%' and EventDay like '$today_8' ORDER BY EventDay");
-            while ($data_content=mysql_fetch_array($query_content2)) {
-            include "inc/content_index.php";
-            }
-          ?>
-        </table>
-        
-        <div class="DayTitle">
-          <span class="DayTitleDate"><?php echo "$today_9";?></span>
-          <span class="DayTitleWeek"><?php echo "星期".$weekarray[$today_9_w];?></span>
-        </div>
-        <table class="table table-hover table-condensed content">
-          <?php
-            $category = @$_GET['category'];
-            $query_content2=mysql_query("SELECT * FROM event WHERE category like '%$category%' and EventDay like '$today_9' ORDER BY EventDay");
-            while ($data_content=mysql_fetch_array($query_content2)) {
-            include "inc/content_index.php";
-            }
-          ?>
-        </table>
+        <?php
+        }
+        ?>
+
       </div>
       <div class="col-md-12">
         footer
