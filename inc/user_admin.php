9aea191b8d1a15aea8263a62f61cafc109084fa9
Ghost <gs.h2004@126.com>
2014-09-02 23:58:06 +0800

    mac_09022356_C

--- inc/user_admin.php
+++ inc/user_admin.php
@@ -2,4 +2,5 @@
           <td><?=$RowUser['id']?></td>
           <td><?=$RowUser['UserName']?></td>
           <td><?=$RowUser['UserGroup']?></td>
+          <td><a href="edit_user.php?id=<?=$RowUser['id']?>">编辑</a></td>
         </tr>
