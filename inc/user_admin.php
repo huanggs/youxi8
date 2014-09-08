1e2680a71aea30ad82a790c7ac9a2dc480b1d791
Huanggs <nightidea@gmail.com>
2014-09-08 14:08:20 +0800

    office_09081408_C

--- inc/user_admin.php
+++ inc/user_admin.php
@@ -1,13 +1,6 @@
         <tr>
-<<<<<<< HEAD
-          <td><?=$RowUser['id']?></td>
-          <td><?=$RowUser['UserName']?></td>
-          <td><?=$RowUser['UserGroup']?></td>
-          <td><a href="edit_user.php?id=<?=$RowUser['id']?>">编辑</a></td>
-=======
           <td align="center"><?=$RowUser['id']?></td>
           <td align="center"><?=$RowUser['UserName']?></td>
           <td align="center"><?=$RowUser['UserGroup']?></td>
           <td align="center"><a href="edit_user.php?id=<?=$RowUser['id']?>">编辑</a></td>
->>>>>>> 61d72e4996c17ec57be5cb24a35c220fce33fd4e
         </tr>
